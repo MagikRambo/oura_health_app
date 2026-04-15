c9ba1b79f67be185ca9828d225db572ace04fabe
mike marambio <mikemarambio@gmail.com>
2026-04-14 23:43:54 -0500

    chore: adding in boostrap tf files for bootstraping the storage acc

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "mike-dev-tfstate-rg"
+    storage_account_name = "mikedevtfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
